2a7d4d9f8e87230bf1509f9e02a66a723a06c68e
fanziwen <120416678@qq.com>
2020-12-29 21:00:16 +0800

    del hda

--- vanTools/scripts/VanTools/temp/setUVSet.py
+++
@@ -1,8 +0,0 @@
-import pymel.core as pm
-
-selList = pm.ls(sl=True)
-uvSet = 1
-for s in selList:
-    mesh = s.getShape()
-    uvNames = mesh.getUVSetNames()
-    pm.polyUVSet(mesh, currentUVSet=True, uvSet=uvNames[uvSet])
